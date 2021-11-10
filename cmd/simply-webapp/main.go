01ff434bb79b00d8c2b90d9d47ac4595ab03fed5
kompilator <roman.skrilioff@gmail.com>
2021-11-10 14:58:32 +0300

    setting up the route

--- cmd/simply-webapp/main.go
+++ cmd/simply-webapp/main.go
@@ -3,30 +3,23 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
+var router *gin.Engine
+
 func main() {
 
+	// Set the router as the default one provided by Gin
 	router := gin.Default()
+
+	// Process the templates at the start so that they dont't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("../../web/template/*")
 
-	router.GET("/", func(c *gin.Context) {
+	// Initialize the routes
+	initializeRoutes()
 
-		// Call the HTML method of the Context to render a template
-		c.HTML(
-			// Set the HTTP status to 200 (ok)
-			http.StatusOK,
-			// Use the index.html template
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-	})
-
+	// Start serving the application
 	router.Run()
 }
