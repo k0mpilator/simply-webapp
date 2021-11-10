a61377acf4c8736eb53afa95f113d437ddab96ea
kompilator <roman.skrilioff@gmail.com>
2021-11-10 11:14:03 +0300

    create application

---
+++ cmd/simply-webapp/main.go
@@ -0,0 +1,32 @@
+// main.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.LoadHTMLGlob("../../web/template/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (ok)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	router.Run()
+}
